9eefba76f0b4f2cc78ccba76fef15c74e82b61bd
Sam Mousa <sam@mousa.nl>
2023-02-14 17:22:12 +0100

    fix: dont use weak reference

--- src/DynamicLogger.php
+++ src/DynamicLogger.php
@@ -6,7 +6,6 @@ namespace yii\psr;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\LoggerTrait;
-use WeakReference;
 
 /**
  * Implements a PSR logger that routes messages to the current Yii Logger.
@@ -15,16 +14,11 @@ final class DynamicLogger implements LoggerInterface
 {
     use LoggerTrait;
     private ?Logger $logger = null;
-    /**
-     * Note that using a weak reference here is merely a good practice.
-     * In reality the ->get() will never return null since the underlying logger doesn't use a weak reference.
-     * @var WeakReference<\yii\log\Logger>|null
-     */
-    private ?WeakReference $yiiLogger = null;
+    private \yii\log\Logger|null $yiiLogger = null;
     private function getLogger(): Logger
     {
-        if (!isset($this->logger, $this->yiiLogger) || \Yii::getLogger() !== $this->yiiLogger->get()) {
-            $this->yiiLogger = WeakReference::create(\Yii::getLogger());
+        if (!isset($this->logger, $this->yiiLogger) || \Yii::getLogger() !== $this->yiiLogger) {
+            $this->yiiLogger = \Yii::getLogger();
             $this->logger = new Logger(\Yii::getLogger());
         }
         return $this->logger;
