ee80c47bfc5c916261fa09287b05e5eceb79ebf9
Alexander Makarov <sam@rmcreative.ru>
2026-09-04 08:45:43 +0300

    fix: remove trailing commas from test expectations

--- tests/DynamicLoggerTest.php
+++ tests/DynamicLoggerTest.php
@@ -15,10 +15,10 @@ final class DynamicLoggerTest extends TestCase
     public function testLoggerUsesCurrent(): void
     {
         $yiiLogger1 = $this->getMockBuilder(YiiLogger::class)->getMock();
-        $yiiLogger1->expects($this->once())->method('log')->with('test1', YiiLogger::LEVEL_INFO, );
+        $yiiLogger1->expects($this->once())->method('log')->with('test1', YiiLogger::LEVEL_INFO);
 
         $yiiLogger2 = $this->getMockBuilder(YiiLogger::class)->getMock();
-        $yiiLogger2->expects($this->once())->method('log')->with('test2', YiiLogger::LEVEL_INFO, );
+        $yiiLogger2->expects($this->once())->method('log')->with('test2', YiiLogger::LEVEL_INFO);
 
         $logger = new DynamicLogger();
         \Yii::setLogger($yiiLogger1);
